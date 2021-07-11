a4adb0d8369f3f8aac128bf0bc70f1d7821dbd99
novel <novelbafagih@student.ub.ac.id>
2021-07-11 17:21:44 +0700

    changing file name

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { addingUser, loginUser, auth } = require('./handler/userPatient');
+const { addingUser, loginUser} = require('./handler/user');
 const {RegisterValidation, LoginValidation} = require('./handler/userValidation');
 const router = express.Router();
 
 
 router.post('/register',RegisterValidation,addingUser);
 router.post('/login',LoginValidation,loginUser);
-router.post('/test',auth);
 
 module.exports = router;
