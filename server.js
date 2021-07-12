fd9855cb18350c4aa27b8f66266ad1865e3f584e
novel <novelbafagih@student.ub.ac.id>
2021-07-12 21:53:14 +0700

    debugging

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const port = process.env.PORT||3000;
 
 async function startApp(){
 	try{
+		console.log(process.env.uri)
 		app.use(express.json());
 		// Routes
 		app.use('/auth',autentication);
